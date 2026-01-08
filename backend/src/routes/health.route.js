d2fdddb1c58c9ae05235e924074f3887490f63ed
Noman Ahmad <nomanahmad9356@gmail.com>
2026-01-08 14:40:58 +0530

    deploy issues fix

--- backend/src/routes/health.route.js
+++ backend/src/routes/health.route.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const {healthCheck} = require("../controllers/health.controller");
+const { healthCheck } = require("../controllers/health.controller");
 
-router.get("/health",healthCheck);
+router.get("/health", healthCheck);
+
+
+router.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "CanvasCraft API is Online",
+        environment: process.env.NODE_ENV || "development",
+        documentation: "https://github.com/Noman-Ahmad25/CanvasCraft"
+    });
+});
+
 module.exports = router;
